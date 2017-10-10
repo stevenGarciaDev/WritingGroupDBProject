ec4ea7f633c4f137c067d1a6bd0dcf8a69365eee
bpham <bpbrianpham@gmail.com>
2017-10-10 09:48:40 -0700

    Reorganized this to make it look nicer

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -3,12 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package writinggroup;
+package controller;
 
 /**
  *
  * @author stevengarcia
  */
-public class View {
+public class Controller {
     
 }
