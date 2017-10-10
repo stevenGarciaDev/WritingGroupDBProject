1e6e8f260ad010b57b2a29b2c421782861b2aa67
stevenGarciaDev <stevgarcia2015@gmail.com>
2017-10-10 08:49:46 -0700

    new files for controller, model, and view added

--- src/writinggroup/View.java
+++ src/writinggroup/View.java
@@ -9,13 +9,6 @@ package writinggroup;
  *
  * @author stevengarcia
  */
-public class WritingGroup {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+public class View {
     
 }
