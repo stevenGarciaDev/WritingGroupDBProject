f8b832edec03207466ab5d5e8168134da5c120b0
stevenGarciaDev <stevgarcia2015@gmail.com>
2017-10-12 10:52:12 -0700

    created constructor and getters and setters for WritingGroup class

--- Project/src/model/WritingGroup.java
+++ Project/src/model/WritingGroup.java
@@ -10,5 +10,55 @@ package model;
  * @author Brian
  */
 public class WritingGroup {
+    private String groupName;
+    private String headWriter;
+    private String yearFormed;
+    private String subject;
+    
+    public WritingGroup() {
+        this.groupName = "";
+        this.headWriter = "";
+        this.yearFormed = "";
+        this.subject = "";
+    }
+    
+    public WritingGroup(String groupName, String headWriter, String yearFormed, String subject) {
+        this.groupName = groupName;
+        this.headWriter = headWriter;
+        this.yearFormed = yearFormed;
+        this.subject = subject;
+    }
+
+    public String getGroupName() {
+        return groupName;
+    }
+
+    public void setGroupName(String groupName) {
+        this.groupName = groupName;
+    }
+
+    public String getHeadWriter() {
+        return headWriter;
+    }
+
+    public void setHeadWriter(String headWriter) {
+        this.headWriter = headWriter;
+    }
+
+    public String getYearFormed() {
+        return yearFormed;
+    }
+
+    public void setYearFormed(String yearFormed) {
+        this.yearFormed = yearFormed;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
     
 }
