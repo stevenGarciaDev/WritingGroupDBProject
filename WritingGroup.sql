a369102fc36ef5c6b2f672ee092587dc281b6901
bpham <brianthongpham@gmail.com>
2017-10-12 09:32:22 -0700

    I added an item into table

--- WritingGroup.sql
+++ WritingGroup.sql
@@ -1,22 +1,13 @@
+drop table WritingGroup;
+drop table Publisher;
+drop table Book;
+
 CREATE TABLE WritingGroup (
 	GroupName VARCHAR(30) NOT NULL,
 	HeadWriter VARCHAR(30),
-	YearFormed YEAR,
+	YearFormed VARCHAR(4),
 	Subject VARCHAR(30),
 	CONSTRAINT writinggroup_pk PRIMARY KEY (GroupName)
-);
-
-CREATE TABLE Book (
-	BookTitle VARCHAR(60) NOT NULL,
-	YearPublished YEAR,
-	NumberPages INTEGER,
-	GroupName VARCHAR(30) NOT NULL,
-	PublisherName VARCHAR(60) NOT NULL,
-	CONSTRAINT book_pk PRIMARY KEY (GroupName, BookTitle, PublisherName),
-	CONSTRAINT writing_groups_fk FOREIGN KEY (groupName)
-	REFERENCES WritingGroup (GroupName),
-	CONSTRAINT publishers_fk FOREIGN KEY (publisherName)
-	REFERENCES Publisher (publisherName)
 );
 
 CREATE TABLE Publisher (
@@ -26,3 +17,19 @@ CREATE TABLE Publisher (
 	PublisherEmail VARCHAR(50),
 	CONSTRAINT pk_publisher PRIMARY KEY (PublisherName)
 );
+
+CREATE TABLE Book (
+	BookTitle VARCHAR(60) NOT NULL,
+	YearPublished VARCHAR(4),
+	NumberPages INTEGER,
+	GroupName VARCHAR(30) NOT NULL,
+	PublisherName VARCHAR(60) NOT NULL,
+	CONSTRAINT book_pk PRIMARY KEY (GroupName, BookTitle, PublisherName),
+	CONSTRAINT writing_groups_fk FOREIGN KEY (groupName)
+	REFERENCES WritingGroup (GroupName),
+	CONSTRAINT publishers_fk FOREIGN KEY (PublisherName)
+	REFERENCES Publisher (PublisherName)
+);
+
+insert into WritingGroup (GroupName, HeadWriter, YearFormed, Subject)
+    values ('Wonder Pen', 'James Jon', '1999', 'English');
