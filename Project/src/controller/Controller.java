74364e71e84e2bf38e8d53585ef9572d35a48954
Brian Pham <brianthongpham@gmail.com>
2017-10-19 11:34:49 -0700

    Updated case 7

--- Project/src/controller/Controller.java
+++ Project/src/controller/Controller.java
@@ -3,6 +3,7 @@ package controller;
 
 
 import java.sql.*;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 /**
@@ -38,6 +39,8 @@ public class Controller {
         else
             return input;
     }
+    
+    
 
     public static void main(String[] args) {
         //Prompt the user for the database name, and the credentials.
@@ -232,20 +235,106 @@ public class Controller {
                     ResultSet rs = preStmt.executeQuery();
 
                     //STEP 5: Extract data from result set
-                    
+                    String dsplyFrmt = "%-30s%-30s%-30s%-30s%-30s%-30s%-30s%-30s%-30s%-30s%-30s";
+                    System.out.printf(dsplyFrmt, "Book Title", "Group Name", "Publisher Name", "Year Published", "Number of Pages", 
+                                    "Head Writer", "Year Formed", "Subject", "Publisher Address", "Publisher Phone", "Publisher Email");
+                    while (rs.next()){
+                        String bBookTitle = rs.getString("BookTitle");
+                        String bGroupName = rs.getString("GroupName");
+                        String bPublisherName = rs.getString("PublisherName");
+                        String bYearPublished = rs.getString("YearPublished");
+                        String bNumberOfPages = rs.getString("NumberPages");
+                        String bHeadWriter = rs.getString("HeadWriter");
+                        String bYearFormed = rs.getString("YearFormed");
+                        String bSubject = rs.getString("Subject");
+                        String bPublisherAddress = rs.getString("PublisherAddress");
+                        String bPublisherPhone = rs.getString("PublisherPhone");
+                        String bPublisherEmail = rs.getString("PublisherEmail");
+                        
+                        System.out.printf(dsplyFrmt, dispNull(bBookTitle), dispNull(bGroupName), dispNull(bPublisherName), 
+                                dispNull(bYearPublished), dispNull(bNumberOfPages), dispNull(bHeadWriter), dispNull(bYearFormed), 
+                                dispNull(bSubject), dispNull(bPublisherAddress), dispNull(bPublisherPhone), dispNull(bPublisherEmail));
+                    }
                     break;
                 }
                 //Insert a new Book
                 case 7:{
+                    System.out.print("Please input the new Book Title: ");
+                    String bookTitle = reader.nextLine();
+                    System.out.print("Please input the new Book year of publish: ");
+                    String yearPublished = reader.nextLine();
+                    System.out.print("Please input the new Book number of pages: ");
+                    int numPages = reader.nextInt();
+                    
+                    //Making the UI List for user to specify
+                    stmt = conn.createStatement();
+                    ArrayList<String> gName = new ArrayList<String>();
+                    String sqlGN = "SELECT GroupName FROM WritingGroup";
+                    ResultSet rsGN = stmt.executeQuery(sqlGN);
+                    int gnNumList = 1;
+                    System.out.println("Group Name List:");
+                    while(rsGN.next()){
+                        System.out.println(gnNumList + ") " + dispNull(rsGN.getString("GroupName")));
+                        gName.add(rsGN.getString("GroupName"));
+                        gnNumList++;
+                    }
+                    
+                    
+                    System.out.print("Please input the number corresponding to the new Book Group Name from above: ");
+                    int gChoiceName = reader.nextInt();
+                    while(gChoiceName < 1 || gChoiceName > gName.size()){
+                        System.out.print("Please enter a valid number from the list above: ");
+                        gChoiceName = reader.nextInt();
+                    }
+                    String groupName = gName.get(gChoiceName - 1);
+
+
+                    //Making the List for Publisher Name
+                    ArrayList<String> pName = new ArrayList<String>();
+                    String sqlPN = "SELECT PublisherName FROM Publisher";
+                    ResultSet rsPN = stmt.executeQuery(sqlPN);
+                    int pnNumList = 1;
+                    System.out.println("Publisher Name List:");
+                    while(rsGN.next()){
+                        System.out.println(pnNumList + ") " + dispNull(rsPN.getString("PublisherName")));
+                        pName.add(rsPN.getString("PublisherName"));
+                        pnNumList++;
+                    }
+                                        
+                    System.out.print("Please input the number corresponding to the new Book Publisher Name from above: ");
+                    int pChoiceName = reader.nextInt();
+                    while(pChoiceName < 1 || pChoiceName > pName.size()){
+                        System.out.print("Please enter a valid number from the list above: ");
+                        pChoiceName = reader.nextInt();
+                    }
+                    String publisherName = pName.get(pChoiceName - 1);
+
+                    
+                    
+                    System.out.println("Creating statement...");
+                    System.out.println("Adding to database...");
+                    String sql;
+                    sql = "INSERT INTO Book(BookTitle, YearPublished, NumberPages, GroupName, PublisherName) VALUES "
+                            + "(?,?,?,?,?);";
+                    preStmt = conn.prepareStatement(sql);
+                    preStmt.setString(1, bookTitle);
+                    preStmt.setString(2, yearPublished);
+                    preStmt.setString(3, Integer.toString(numPages));
+                    preStmt.setString(4, groupName);
+                    preStmt.setString(5, publisherName);                    
+                    ResultSet rs = preStmt.executeQuery();
+                    
                     
                     break;
                 }
                 //Insert a new Publisher (followed by a replacing of an old Publisher)
                 case 8:{
+                    
                     break;
                 }
                 //remove a book
                 default:{
+                    
                     break;
                 }
             };
