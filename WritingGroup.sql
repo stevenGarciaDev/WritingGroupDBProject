6b505f59397f53394c478fe79790f07deca1822d
stevenGarciaDev <stevgarcia2015@gmail.com>
2017-10-19 10:07:04 -0700

    Insert data for WritingGroup and Book tables

--- WritingGroup.sql
+++ WritingGroup.sql
@@ -74,3 +74,34 @@ INSERT INTO Publisher(publisherName, publisherAddress,
 	('Tupac', '1010 Harlem, New York', '9999999999', '2pac@yolo.com'),
 	('Willy Wonka', '7680 Chocolate Street, Vermont', '5555555555', 'willyWonka@myspace.com'),
 	('Bernie Sanders', '0000 Bottom 99 Persent, Maryland', '0000000000', 'topOnePercent@myspace.com');
+
+INSERT INTO WrtingGroup(GroupName, HeadWriter, YearFormed, Subject) VALUES
+	('ASAP MOB', 'Asap Rocky', '2011', 'Hip Hop'),
+	('Wu Tang Clan', 'RZA', '1990', 'Hip Hop'),
+	('Michelin Guide', 'Some French Guy', '1900', 'Restaurants'),
+	('Hacker News', 'Paul Graham', '2000', 'Technology');
+
+INSERT INTO Book(groupName, bookTitle, publisherName, YearPublished, NumberPages) VALUES
+	('ASAP MOB', 'So you want to be a rapper', 'Prestige Worldwide', '2011', 111),
+	('ASAP MOB', 'So you want to be a gangsta', 'Tupac', '2014', 222),
+	('Wu Tang Clan', 'Rapping for Fun and profit', 'Tupac', '1991', 121),
+	('Wu Tang Clan', 'Rapping For Dummies', 'Prestige Worldwide', '1990', 131),
+
+	('Wu Tang Clan', 'So you want to run for president', 'Prestige Worldwide', '1996', 131),
+	('Michelin Guide', 'The Art of French Cooking', 'Drake', '1980', 421),
+	('Michelin Guide', 'How to Make Good BBQ for dummies', 'Drake', '2017', 412),
+	('Michelin Guide', 'How to make french toast for dummies', 'Willy Wonka', '2013', 312),
+
+	('Michelin Guide', 'How to flip pancakes for dummies', 'Willy Wonka', '2002', 267),
+	('Hacker News', 'How to make an app', 'Bernie Sanders', '2016', 211),
+	('Hacker News', 'How to code for dummies', 'Drake', '2015', 298),
+	('Hacker News', 'how to flip pizza for dummies', 'Harry Potter', '2009', 165);
+
+
+
+
+
+
+
+
+
